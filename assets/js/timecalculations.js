58e83eae170d8aed8dd4c0151e6c11523e18790d
Sarika <sarika@ngoyal.com>
2019-04-04 09:44:32 -0700

    Fixed time calculation bugs

--- assets/js/timecalculations.js
+++ assets/js/timecalculations.js
@@ -3,7 +3,7 @@ function next_train(first_train, frequency) {
 	let now = moment()
     let duration = moment.duration(now.diff(start_time));
     let totalMinutesSinceFirstTrain = Math.abs(duration.asMinutes());
-    console.log("Math.abs(duration.asMinutes())" + Math.abs(duration.asMinutes()))
+    //console.log("Math.abs(duration.asMinutes())" + Math.abs(duration.asMinutes()))
     let minute
     
     //if train not started yet
@@ -13,15 +13,13 @@ function next_train(first_train, frequency) {
     //Find minutes past since last train
     
     minute = totalMinutesSinceFirstTrain % frequency
-
-    console.log("totalMinutesSinceFirstTrain % frequency" + minute)
-    
+   
     //Find how many minutes to go
     //add duration plus time to next train
 
     let howMuchtoAdd = frequency - minute
 
-    console.log('howMuchtoAdd' + howMuchtoAdd)
+    //console.log('howMuchtoAdd' + howMuchtoAdd)
     howMuchtoAdd = howMuchtoAdd + totalMinutesSinceFirstTrain
     
     if (now.isBefore(start_time)) 
@@ -32,28 +30,29 @@ function next_train(first_train, frequency) {
 function minsleft(first_train, frequency) {
 
 	let start_time = moment(first_train, 'HH:mm A');
-  let now = moment()
+    let now = moment()
 
   //get duration between the 2 times, if negative, make positive
   let duration = moment.duration(now.diff(start_time));
 
-  //get duration in minutes
-  let minute = Math.abs(duration.asMinutes());
+    //get duration in minutes
+  let minute = Math.abs(duration.asMinutes())
 
+ 
   //If train not started 
   if (now.isBefore(start_time)) 
   {
       //return rounded duration in minutes
-      return Math.round(minute)
+      return Math.ceil(minute);
   }
+  minute = Math.abs(parseInt(duration.asMinutes()));
   
     //Find minutes past since last train
     minute = minute % frequency
-    
+     
     //minutes to next train
-    minute = Math.round(frequency - minute)
-
-   
+    minute = frequency - minute
+     
     if (minute === 0)
     return "< min"
     else 
